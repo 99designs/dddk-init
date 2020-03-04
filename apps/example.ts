823323a6e2b3d01dd5c8956ae8983a6ecfd67fc4
Adam <adam@vektah.net>
2020-03-04 17:49:06 +1100

    include custom component

--- apps/example.ts
+++ apps/example.ts
@@ -5,12 +5,17 @@ import fargate from "dddk/shared/fargate";
 import healthcheck from "dddk/shared/healthcheck";
 import alb from "dddk/shared/alb";
 import team from "../team";
+import custom from "../components/custom";
 
 export default new App({
   name: "ExampleApp",
   team: team.fooBars,
   components: [
     Group("Group 1", [healthcheck("http://example.com"), alb("example-app")]),
-    Group("Group 2", [rds("example-app"), fargate("example-app")]),
+    Group("Group 2", [
+      rds("example-app"),
+      fargate("example-app"),
+      custom("example-app"),
+    ]),
   ],
 });
